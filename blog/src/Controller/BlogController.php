eba9d4bef4f8032307632e0928b405b21fafdc81
Alexandre Pasquier <alexandre.pasquier91@gmail.com>
2019-05-15 11:10:49 +0200

    quète symfony 4

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -4,22 +4,20 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog_index")
+     * @Route("/blog/show/{slug}",
+     * requirements={"slug"="[a-z0-9-]+"},
+     * defaults={"slug"="Article Sans Titre"},
+     * name="blog_article")
      */
-
-    public function index()
+    public function show($slug)
     {
-        return $this->render('blog/index.html.twig', [
-            'owner' => 'Thomas',
-        ]);
+        $slug= ucwords(str_replace('-', ' ', $slug ));
+        return $this->render('blog/show.html.twig', ['slug' => $slug]);
     }
-
 }
